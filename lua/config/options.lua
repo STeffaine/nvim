428b24e420a94376387234ec2e79f55237a65cb3
STeffaine <teffaine.stephane@gmail.com>
2024-04-09 14:25:55 -0500

    Add files via upload

---
+++ lua/config/options.lua
@@ -0,0 +1,9 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- Always keep 8 lines above/below
+vim.opt.scrolloff = 8
+
+-- Always have signcolumn, not only when a plugin wants it
+vim.opt.signcolumn = "yes"
