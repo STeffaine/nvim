154d0ba393c301aba569e243e2b05286f86cc0ad
STeffaine <s@teffaine.ca>
2024-06-24 18:20:34 -0500

    add tagclosing

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,17 +1,25 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        highlight = { enable = true },
-        indent = { enable = true },
-        ensure_installed = {
-          "lua",
-          "javascript",
-        },
-      })
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				highlight = { enable = true },
+				indent = { enable = true },
+				ensure_installed = {
+					"lua",
+					"javascript",
+				},
+			})
+		end,
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		opts = {
+			enable_close = true,
+			enable_rename = true,
+			enable_close_on_slash = true,
+		},
+	},
 }
