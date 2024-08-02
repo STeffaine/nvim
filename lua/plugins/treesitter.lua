95415e9f96cd876fb341ff2dcb9f88692b257ed5
STeffaine <s@teffaine.ca>
2024-08-02 09:57:19 -0500

    chore: indenting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,23 +1,23 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			local config = require("nvim-treesitter.configs")
-			config.setup({
-				highlight = { enable = true },
-				indent = { enable = true },
-				ensure_installed = {
-					"lua",
-					"javascript",
-				},
-			})
-		end,
-	},
-	{
-		"windwp/nvim-ts-autotag",
-    config = function ()
-      local config = require('nvim-ts-autotag')
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+        highlight = { enable = true },
+        indent = { enable = true },
+        ensure_installed = {
+          "lua",
+          "javascript",
+        },
+      })
+    end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    config = function()
+      local config = require("nvim-ts-autotag")
       config.setup({
         opts = {
           enable_close = true,
@@ -25,6 +25,6 @@ return {
           enable_close_on_slash = true,
         },
       })
-    end
-	},
+    end,
+  },
 }
