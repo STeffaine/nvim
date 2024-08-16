c834d8358a248eb8a22bcbafd9489b9cfe9c342b
STeffaine <s@teffaine.ca>
2024-08-16 12:05:03 -0500

    add illuminate.nvim

---
+++ lua/plugins/illuminate.lua
@@ -0,0 +1,20 @@
+return {
+  'RRethy/vim-illuminate',
+  config = function()
+    require('illuminate').configure({
+      providers = {
+        'lsp',
+        'treesitter',
+        'regex',
+      },
+      delay = 100,
+      filetype_overrides = {},
+      filetype_denylist = {},
+      under_curor = true,
+      min_count_to_highlight = 1,
+      case_insensitive_regex = true,
+
+      should_enable = function(bufnr) return true end,
+    })
+  end,
+}
