a5f2550c756a340e80cecc8601fdc259e3bbcac3
STeffaine <s@teffaine.ca>
2024-09-19 14:15:43 -0500

    add some stuff for go lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -30,8 +30,13 @@ return {
         capabilities = capabilities,
         cmd = { "gopls" },
         filetypes = { "go", "gomod", "gowork", "gotmpl" },
+        root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
         settings = {
           completeUnimported = true,
+          usePlaceholders = true,
+          analyses = {
+            unusedparams = true,
+          },
         },
       })
 
