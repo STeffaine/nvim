21a21cd8a21b0a241cb74b638bf45a32aef37048
STeffaine <steffaine@DESKTOP-LLDVJTA>
2024-09-25 17:48:16 -0500

    add rest.nvim : REQUIRES 0.10.1

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,15 +6,15 @@ vim.keymap.set("n", "<leader>o", "<cmd>Ex %:h<CR>", { desc = "Open vim explorer 
 vim.keymap.set("n", "<leader>c", "<cmd>bw!<CR>", { desc = "Write and close current buffer" })
 vim.keymap.set("n", "<leader>[", "<cmd>bprev<CR>", { desc = "Go to previous buffer" })
 vim.keymap.set("n", "<leader>]", "<cmd>bnext<CR>", { desc = "Go to next buffer" })
-vim.keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "quit file"})
-vim.keymap.set("n", "<leader>Q", "<cmd>q!<CR>", { desc = "quit file force"})
+vim.keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "quit file" })
+vim.keymap.set("n", "<leader>Q", "<cmd>q!<CR>", { desc = "quit file force" })
 
 -- Windows
-vim.keymap.set("n", "<C-h>", "<cmd>wincmd h<CR>", { desc = "Select window to left"})
-vim.keymap.set("n", "<C-j>", "<cmd>wincmd j<CR>", { desc = "Select window to left"})
-vim.keymap.set("n", "<C-k>", "<cmd>wincmd k<CR>", { desc = "Select window to left"})
-vim.keymap.set("n", "<C-l>", "<cmd>wincmd l<CR>", { desc = "Select window to left"})
-vim.keymap.set("n", "<leader>p", "<cmd>Lexplore<CR>", { desc = "Open Explore in VSplit"})
+vim.keymap.set("n", "<C-h>", "<cmd>wincmd h<CR>", { desc = "Select window to left" })
+vim.keymap.set("n", "<C-j>", "<cmd>wincmd j<CR>", { desc = "Select window to left" })
+vim.keymap.set("n", "<C-k>", "<cmd>wincmd k<CR>", { desc = "Select window to left" })
+vim.keymap.set("n", "<C-l>", "<cmd>wincmd l<CR>", { desc = "Select window to left" })
+vim.keymap.set("n", "<leader>p", "<cmd>Lexplore<CR>", { desc = "Open Explore in VSplit" })
 
 -- Keymaps for finding things
 local builtin = require("telescope.builtin")
@@ -38,55 +38,75 @@ local dap = require("dap")
 vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "DAP Toggle Breakpoint" })
 vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "DAP Continue" })
 
+-- Rest.nvim
+vim.keymap.set("n", "<leader>ro", "<cmd>rightb Rest open<cr>", { desc = "Open Rest Panel" })
+vim.keymap.set("n", "<leader>rr", "<cmd>Rest run<cr>", { desc = "REST: Run request under the cursor" })
+vim.keymap.set("n", "<leader>rl", "<cmd>Rest last<cr>", { desc = "REST: Run last request" })
+vim.keymap.set("n", "<leader>rg", "<cmd>Rest logs<cr>", { desc = "REST: Run last request" })
+vim.keymap.set("n", "<leader>rc", "<cmd>Rest cookies<cr>", { desc = "REST: Edit cookies file" })
+vim.keymap.set(
+  "n",
+  "<leader>res",
+  "<cmd>Rest env show<cr>",
+  { desc = "REST: Show dotenv file registered to current .http file" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>rec",
+  "<cmd>Rest env select<cr>",
+  { desc = "REST: Select & register .env file with vim.ui.select()" }
+)
+vim.keymap.set("n", "<leader>rep", "<cmd>Rest env set<cr>", { desc = "REST: Register .env file to current .http file" })
+
 -- Git Stuff
 vim.keymap.set("n", "<leader>sl", "<cmd>LazyGit<cr>", { desc = "Open LazyGit" })
 require("gitsigns").setup({
-	on_attach = function(bufnr)
-		local gitsigns = require("gitsigns")
+  on_attach = function(bufnr)
+    local gitsigns = require("gitsigns")
 
-		local function map(mode, l, r, opts)
-			opts = opts or {}
-			opts.buffer = bufnr
-			vim.keymap.set(mode, l, r, opts)
-		end
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
 
-		-- Navigation
-		map("n", "]c", function()
-			if vim.wo.diff then
-				vim.cmd.normal({ "]c", bang = true })
-			else
-				gitsigns.nav_hunk("next")
-			end
-		end)
+    -- Navigation
+    map("n", "]c", function()
+      if vim.wo.diff then
+        vim.cmd.normal({ "]c", bang = true })
+      else
+        gitsigns.nav_hunk("next")
+      end
+    end)
 
-		map("n", "[c", function()
-			if vim.wo.diff then
-				vim.cmd.normal({ "[c", bang = true })
-			else
-				gitsigns.nav_hunk("prev")
-			end
-		end)
+    map("n", "[c", function()
+      if vim.wo.diff then
+        vim.cmd.normal({ "[c", bang = true })
+      else
+        gitsigns.nav_hunk("prev")
+      end
+    end)
 
-		-- Actions
-		map("n", "<leader>ss", gitsigns.stage_hunk, { desc = "Stage Hunk Under Cursor" })
-		map("n", "<leader>sr", gitsigns.reset_hunk, { desc = "Reset Hunk Under Cursor" })
-		map("v", "<leader>ss", function()
-			gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-		end, { desc = "Stage Selected Hunk" })
-		map("v", "<leader>sr", function()
-			gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-		end, { desc = "Reset Selected Hunk" })
-		map("n", "<leader>sS", gitsigns.stage_buffer, { desc = "Stage Buffer" })
-		map("n", "<leader>su", gitsigns.undo_stage_hunk, { desc = "Undo Stage Hunk" })
-		map("n", "<leader>sR", gitsigns.reset_buffer, { desc = "Reset Buffer Hunk" })
-		map("n", "<leader>sp", gitsigns.preview_hunk, { desc = "Preview Hunk" })
-		map("n", "<leader>sB", function()
-			gitsigns.blame_line({ full = true })
-		end, { desc = "Show Full Git Blame" })
-		map("n", "<leader>sb", gitsigns.toggle_current_line_blame, { desc = "Toggle inline Git Blame" })
-		map("n", "<leader>sd", gitsigns.diffthis, { desc = "Diff This" })
-		map("n", "<leader>sD", function()
-			gitsigns.diffthis("~")
-		end)
-	end,
+    -- Actions
+    map("n", "<leader>ss", gitsigns.stage_hunk, { desc = "Stage Hunk Under Cursor" })
+    map("n", "<leader>sr", gitsigns.reset_hunk, { desc = "Reset Hunk Under Cursor" })
+    map("v", "<leader>ss", function()
+      gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+    end, { desc = "Stage Selected Hunk" })
+    map("v", "<leader>sr", function()
+      gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+    end, { desc = "Reset Selected Hunk" })
+    map("n", "<leader>sS", gitsigns.stage_buffer, { desc = "Stage Buffer" })
+    map("n", "<leader>su", gitsigns.undo_stage_hunk, { desc = "Undo Stage Hunk" })
+    map("n", "<leader>sR", gitsigns.reset_buffer, { desc = "Reset Buffer Hunk" })
+    map("n", "<leader>sp", gitsigns.preview_hunk, { desc = "Preview Hunk" })
+    map("n", "<leader>sB", function()
+      gitsigns.blame_line({ full = true })
+    end, { desc = "Show Full Git Blame" })
+    map("n", "<leader>sb", gitsigns.toggle_current_line_blame, { desc = "Toggle inline Git Blame" })
+    map("n", "<leader>sd", gitsigns.diffthis, { desc = "Diff This" })
+    map("n", "<leader>sD", function()
+      gitsigns.diffthis("~")
+    end)
+  end,
 })
