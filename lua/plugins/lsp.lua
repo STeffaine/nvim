32ccd0590d80280b6b6277ee427552fd52a7fbfe
STeffaine <84108573+STeffaine@users.noreply.github.com>
2024-12-21 12:11:21 -0600

    fix diagnositcs for eslint

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -99,6 +99,7 @@ return {
       local null_ls = require("null-ls")
 
       local formatting = null_ls.builtins.formatting
+      local diagnostics = null_ls.builtins.diagnostics
 
       null_ls.setup({
         sources = {
