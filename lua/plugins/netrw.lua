62c46238f7b70e276a01efcedac6f2fbb1b48429
STeffaine <84108573+STeffaine@users.noreply.github.com>
2025-01-04 09:09:20 -0600

    enable netrw

--- lua/plugins/netrw.lua
+++ lua/plugins/netrw.lua
@@ -1,16 +1,16 @@
 return {
-  -- {
-  --   "prichrd/netrw.nvim",
-  --   event = "BufRead",
-  --   lazy = true,
-  --   opts = {},
-  --   dependencies = {
-  --     "kyazdani42/nvim-web-devicons",
-  --   },
-  --   config = function()
-  --     require("netrw").setup({
-  --       use_devicons = true,
-  --     })
-  --   end,
-  -- },
+  {
+    "prichrd/netrw.nvim",
+    event = "BufRead",
+    lazy = true,
+    opts = {},
+    dependencies = {
+      "kyazdani42/nvim-web-devicons",
+    },
+    config = function()
+      require("netrw").setup({
+        use_devicons = true,
+      })
+    end,
+  },
 }
