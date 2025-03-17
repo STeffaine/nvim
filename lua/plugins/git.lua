7a6e65df20ce70a169d54bf6b4c57ee4757b88ed
STeffaine <84108573+STeffaine@users.noreply.github.com>
2025-03-17 02:23:39 -0500

    git signs, remove blame delay when on. And show word diff in diff

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -15,7 +15,7 @@ return {
         signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
         numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
         linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
-        word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+        word_diff = true, -- Toggle with `:Gitsigns toggle_word_diff`
         watch_gitdir = {
           follow_files = true,
         },
@@ -25,7 +25,7 @@ return {
         current_line_blame_opts = {
           virt_text = true,
           virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-          delay = 1000,
+          delay = 0,
           ignore_whitespace = false,
           virt_text_priority = 100,
         },
