d396182af23b397be608c356692bdc4125815124
STeffaine <84108573+STeffaine@users.noreply.github.com>
2026-04-28 15:06:05 -0500

    added some settings, looking at the upstream ref

--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -15,10 +15,13 @@ return {
       under_curor = true,
       min_count_to_highlight = 1,
       case_insensitive_regex = true,
+      large_file_cutoff = 1000,
 
       should_enable = function(bufnr)
         return true
       end,
+
+      case_insensitive_regex = false,
     })
   end,
 }
