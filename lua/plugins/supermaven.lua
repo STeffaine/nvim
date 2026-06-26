bd6416ca234cba79c165bd3e81e4616ed6887614
STeffaine <84108573+STeffaine@users.noreply.github.com>
2026-06-25 22:32:51 -0500

    supermaven keys

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -2,6 +2,12 @@ return {
   "supermaven-inc/supermaven-nvim",
   event = "BufRead",
   config = function()
-    require("supermaven-nvim").setup({})
+    require("supermaven-nvim").setup({
+      keymaps = {
+        accept_suggestion = "<Tab>",
+        clear_suggestion = "<C-[>",
+        accept_word = "<C-h>",
+      },
+    })
   end,
 }
